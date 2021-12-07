a1c27e8a8ac160a3072158329fcdb2e4232e4a3d
FaikYY <yazilimcifaik@gmail.com>
2021-12-07 01:23:59 -0600

    watermark added

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,14 @@
                                   |__/                                        
 -->
 <template>
-  <div 
-    id="app" 
-    :class="typeof weather.main != 'undefined' && checkWeatherForBackground">
+  <div
+    id="app"
+    :class="typeof weather.main != 'undefined' && checkWeatherForBackground"
+  >
     <main>
       <SearchBar />
       <WeatherInfo />
+      <a href="https://vuejs-profile-card.netlify.app/" id="watermark">Created by Faik Yesilyaprak</a>
     </main>
   </div>
 </template>
@@ -36,44 +38,46 @@
                                   |__/                
 -->
 <script>
-import SearchBar from './components/SearchBar.vue';
-import WeatherInfo from './components/WeatherInfo.vue';
+import SearchBar from "./components/SearchBar.vue";
+import WeatherInfo from "./components/WeatherInfo.vue";
 
 export default {
   components: { SearchBar, WeatherInfo },
-  name: 'app',
-    computed:{
-     weather: {
+  name: "app",
+  computed: {
+    weather: {
       get() {
         return this.$store.state.weather;
       },
     },
-    checkWeatherForBackgroundFromStore(){
-      return this.$store.dispatch("checkWeatherForBackground")
+    checkWeatherForBackgroundFromStore() {
+      return this.$store.dispatch("checkWeatherForBackground");
     },
-    checkWeatherForBackground(){
-      if(this.$store.state.weather.weather[0].main == 'Clear'){
-        console.log("Clear weather background")
-        return 'clear'
+    checkWeatherForBackground() {
+      if (this.$store.state.weather.weather[0].main == "Clear") {
+        console.log("Clear weather background");
+        return "clear";
+      } else if (
+        this.$store.state.weather.weather[0].main == "Clouds" &&
+        Math.round(this.$store.state.weather.main.temp) >= 16
+      ) {
+        console.log("Clouds weather background");
+        return "clouds-warm";
+      } else if (
+        this.$store.state.weather.weather[0].main == "Clouds" &&
+        Math.round(this.$store.state.weather.main.temp) < 16
+      ) {
+        console.log("Clouds weather background");
+        return "clouds-cold";
+      } else if (this.$store.state.weather.weather[0].main == "Snow") {
+        console.log("Snow weather background");
+        return "snow";
+      } else {
+        return "";
       }
-      else if(this.$store.state.weather.weather[0].main == 'Clouds' && Math.round(this.$store.state.weather.main.temp) >= 16){
-        console.log("Clouds weather background")
-        return 'clouds-warm'
-      }
-      else if(this.$store.state.weather.weather[0].main == 'Clouds' && Math.round(this.$store.state.weather.main.temp) < 16){
-        console.log("Clouds weather background")
-        return 'clouds-cold'
-      }
-      else if(this.$store.state.weather.weather[0].main == 'Snow'){
-        console.log("Snow weather background")
-        return 'snow'
-      }
-      else{
-        return ''
-      }
-    }
+    },
   },
-}
+};
 </script>
 
 <!-- 
@@ -97,40 +101,49 @@ export default {
 }
 
 body {
-  font-family: 'montserrat', sans-serif;
+  font-family: "montserrat", sans-serif;
 }
 
 #app {
-  background-image: url('./assets/default.jpg');
+  background-image: url("./assets/default.jpg");
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
 }
 
-#app.clouds-warm{
-  background-image: url('./assets/clouds-warm.jpg');
+#app.clouds-warm {
+  background-image: url("./assets/clouds-warm.jpg");
 }
 
-#app.clouds-cold{
-  background-image: url('./assets/clouds-cold.jpg');
+#app.clouds-cold {
+  background-image: url("./assets/clouds-cold.jpg");
 }
 
-#app.snow{
-  background-image: url('./assets/snow.jpg');
+#app.snow {
+  background-image: url("./assets/snow.jpg");
 }
 
-#app.clear{
-  background-image: url('./assets/clear.jpg');
+#app.clear {
+  background-image: url("./assets/clear.jpg");
 }
 main {
   min-height: 100vh;
   padding: 25px;
 
-  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+#watermark {
+  font-size: 1.5rem;
+  position: fixed;
+  bottom: 50px;
+  right: 25px;
+  opacity: 0.75;
+  z-index: 99;
+  color: rgb(225, 225, 225);
 }
 </style>
-
-
-// TODO: Place the search bar to the bottom when its on mobile
-// TODO: Seperate the components
-// TODO: Fix the background on desktop mode
